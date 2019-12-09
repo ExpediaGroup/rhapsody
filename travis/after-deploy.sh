56d120cdd037703961691d7030067b7d04c26567
spierce <spierce@homeaway.com>
2019-12-09 15:55:02 -0600

    Keep trying to make PGP signing work

--- travis/after-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Removing existing keyrings"
-rm -rf $GPG_DIR
--- travis/before-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]; then
-  echo "Installing keyrings"
-  mkdir -p $GPG_DIR
-  openssl aes-256-cbc -K $encrypted_cd1337d2ab17_key -iv $encrypted_cd1337d2ab17_iv -in travis/gpg.asc.enc -out ${GPG_DIR}/gpg.asc -d
-fi
