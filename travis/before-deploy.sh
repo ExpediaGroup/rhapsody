12fb05d20b9184d55e140b18a2c9a8963a2cd938
spierce <spierce@homeaway.com>
2019-12-09 15:45:40 -0600

    Remove (possibly) unneeded build scripting

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -6,6 +6,4 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]; then
   echo "Installing keyrings"
   mkdir -p $GPG_DIR
   openssl aes-256-cbc -K $encrypted_cd1337d2ab17_key -iv $encrypted_cd1337d2ab17_iv -in travis/gpg.asc.enc -out ${GPG_DIR}/gpg.asc -d
-  echo $GPG_PUBLIC_KEYS | base64 --decode >> ${GPG_DIR}/pubring.gpg
-  echo $GPG_SECRET_KEYS | base64 --decode >> ${GPG_DIR}/secring.gpg
 fi
