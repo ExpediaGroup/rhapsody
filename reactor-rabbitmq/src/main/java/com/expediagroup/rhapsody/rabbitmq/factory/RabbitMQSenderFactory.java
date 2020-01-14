0c4f9dd46397fd54a34a008fa42e66dfc633a64a
Sage Pierce <sapierce@expediagroup.com>
2020-01-14 17:10:00 -0600

    [#30]Update to latest Reactor-RabbitMQ; Acknowledgeable RabbitMQ Sending (#33)

--- reactor-rabbitmq/src/main/java/com/expediagroup/rhapsody/rabbitmq/factory/RabbitMQSenderFactory.java
+++ reactor-rabbitmq/src/main/java/com/expediagroup/rhapsody/rabbitmq/factory/RabbitMQSenderFactory.java
@@ -17,19 +17,23 @@ package com.expediagroup.rhapsody.rabbitmq.factory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.expediagroup.rhapsody.api.Acknowledgeable;
 import com.expediagroup.rhapsody.rabbitmq.message.RabbitMessage;
 import com.expediagroup.rhapsody.rabbitmq.message.RabbitMessageSendInterceptor;
 import com.expediagroup.rhapsody.rabbitmq.serde.BodySerializer;
 import com.expediagroup.rhapsody.util.ConfigLoading;
 
 import reactor.core.publisher.Flux;
+import reactor.rabbitmq.CorrelableOutboundMessage;
 import reactor.rabbitmq.OutboundMessage;
 import reactor.rabbitmq.OutboundMessageResult;
+import reactor.rabbitmq.SendOptions;
 import reactor.rabbitmq.Sender;
 import reactor.rabbitmq.SenderOptions;
 
@@ -40,6 +44,11 @@ public class RabbitMQSenderFactory<T> {
     private static final boolean DEFAULT_RESUBSCRIBE_ON_ERROR = true;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQSenderFactory.class);
+
+    private static final SendOptions DEFAULT_SEND_OPTIONS = new SendOptions();
+
+    private static final SendOptions DEFAULT_ACKNOWLEDGEABLE_SEND_OPTIONS = new SendOptions()
+        .exceptionHandler(RabbitMQSenderFactory::handleAcknowledgeableSendException);
 
     private final Sender sender;
 
@@ -57,11 +66,20 @@ public class RabbitMQSenderFactory<T> {
         this.resubscribeOnError = ConfigLoading.load(properties, RESUBSCRIBE_ON_ERROR_CONFIG, Boolean::valueOf, DEFAULT_RESUBSCRIBE_ON_ERROR);
     }
 
-    public Flux<OutboundMessageResult> send(Publisher<RabbitMessage<T>> messages) {
+    public Flux<Acknowledgeable<OutboundMessageResult<CorrelableOutboundMessage<T>>>>
+    sendAcknowledgeable(Publisher<Acknowledgeable<RabbitMessage<T>>> acknowledgeables) {
+        return Flux.from(acknowledgeables)
+            .map(interceptors.isEmpty() ? Function.identity() : Acknowledgeable.mapping(this::applyInterceptors))
+            .map(message -> serialize(message, Acknowledgeable::get, acknowledgeable -> acknowledgeable.map(RabbitMessage::getBody)))
+            .transform(serializedMessages -> sendSerialized(serializedMessages, DEFAULT_ACKNOWLEDGEABLE_SEND_OPTIONS))
+            .map(this::toAcknowledgeableResult);
+    }
+
+    public Flux<OutboundMessageResult<CorrelableOutboundMessage<T>>> send(Publisher<RabbitMessage<T>> messages) {
         return Flux.from(messages)
             .map(this::applyInterceptors)
-            .map(this::serialize)
-            .transform(this::sendSerialized);
+            .map(message -> serialize(message, Function.identity(), RabbitMessage::getBody))
+            .transform(serializedMessages -> sendSerialized(serializedMessages, DEFAULT_SEND_OPTIONS));
     }
 
     private RabbitMessage<T> applyInterceptors(RabbitMessage<T> rabbitMessage) {
@@ -71,15 +89,33 @@ public class RabbitMQSenderFactory<T> {
         return rabbitMessage;
     }
 
-    private OutboundMessage serialize(RabbitMessage<T> message) {
+    private <W, C> CorrelableOutboundMessage<C> serialize(W wrapper, Function<W, RabbitMessage<T>> messageExtractor, Function<W, C> correlationExtractor) {
+        RabbitMessage<T> message = messageExtractor.apply(wrapper);
         byte[] serializedBody = bodySerializer.serialize(message.getBody());
-        return new OutboundMessage(message.getExchange(), message.getRoutingKey(), message.getProperties(), serializedBody);
+        return new CorrelableOutboundMessage<>(
+            message.getExchange(), message.getRoutingKey(), message.getProperties(), serializedBody, correlationExtractor.apply(wrapper));
     }
 
-    private Flux<OutboundMessageResult> sendSerialized(Flux<OutboundMessage> messages) {
-        return messages
-            .transform(sender::sendWithPublishConfirms)
+    private <M extends OutboundMessage> Flux<OutboundMessageResult<M>> sendSerialized(Flux<M> messages, SendOptions sendOptions) {
+        return sender.sendWithTypedPublishConfirms(messages, sendOptions)
             .doOnError(error -> LOGGER.warn("An Error was encountered while trying to send to RabbitMQ. resubscribeOnError={}", resubscribeOnError, error))
             .retry(error -> resubscribeOnError);
+    }
+
+    private Acknowledgeable<OutboundMessageResult<CorrelableOutboundMessage<T>>>
+    toAcknowledgeableResult(OutboundMessageResult<CorrelableOutboundMessage<Acknowledgeable<T>>> outboundMessageResult) {
+        CorrelableOutboundMessage<Acknowledgeable<T>> outboundMessage = outboundMessageResult.getOutboundMessage();
+        return outboundMessage.getCorrelationMetadata().map(data -> new OutboundMessageResult<>(
+            toCorrelableOutboundMessage(outboundMessage, data), outboundMessageResult.isAck(), outboundMessageResult.isReturned()));
+    }
+
+    private CorrelableOutboundMessage<T> toCorrelableOutboundMessage(OutboundMessage message, T correlationMetadata) {
+        return new CorrelableOutboundMessage<>(
+            message.getExchange(), message.getRoutingKey(), message.getProperties(), message.getBody(), correlationMetadata);
+    }
+
+    //TODO This is an ugly result of SendContext not being parameterized on `sendWithTypedPublishConfirms`
+    private static void handleAcknowledgeableSendException(Sender.SendContext sendContext, Exception error) {
+        Acknowledgeable.class.cast(CorrelableOutboundMessage.class.cast(sendContext.getMessage()).getCorrelationMetadata()).nacknowledge(error);
     }
 }
