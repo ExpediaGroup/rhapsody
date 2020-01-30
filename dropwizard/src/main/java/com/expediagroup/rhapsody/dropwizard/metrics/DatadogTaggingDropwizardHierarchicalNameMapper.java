bff6d847ad6d8cf82ca79423f4b03c2794975c50
Sage Pierce <sapierce@expediagroup.com>
2020-01-30 12:55:01 -0600

    [#41]Add Dropwizard Module; Move Dropwizard code there (#42)

--- dropwizard/src/main/java/com/expediagroup/rhapsody/dropwizard/metrics/DatadogTaggingDropwizardHierarchicalNameMapper.java
+++ dropwizard/src/main/java/com/expediagroup/rhapsody/dropwizard/metrics/DatadogTaggingDropwizardHierarchicalNameMapper.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.expediagroup.rhapsody.core.metrics;
+package com.expediagroup.rhapsody.dropwizard.metrics;
 
 import java.util.List;
 import java.util.stream.Collectors;
